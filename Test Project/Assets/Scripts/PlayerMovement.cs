0f345bcf7a6337200ce65889f3daa8c1ffd3b66a
MysteriousShep <95491119+MysteriousShep@users.noreply.github.com>
2022-10-17 10:20:43 -0700

    Jumping???

--- Test Project/Assets/Scripts/PlayerMovement.cs
+++ Test Project/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5.0f;
     public Vector2 movement;
     public Rigidbody2D rb;
+    private float jumpFrame = 0.0f;
+    private float yVelocity;
     // Update is called once per frame
     void Update()
     {
@@ -16,6 +18,18 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(movement.x*speed,movement.y*speed);
+        yVelocity = rb.velocity.y;
+        if (jumpFrame < 10 && movement.y > 0) {
+            jumpFrame += 1;
+            yVelocity = 10.0f;
+        } else if (jumpFrame < 10 && jumpFrame > 0) {
+            jumpFrame -= 1;
+        }
+        rb.velocity = new Vector2(movement.x*speed,yVelocity);
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        jumpFrame = 10.0f;
     }
 }
