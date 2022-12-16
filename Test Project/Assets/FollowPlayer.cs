4b8c87779386db4f30d41a34393011f1b6bff20f
MysteriousShep <95491119+MysteriousShep@users.noreply.github.com>
2022-12-16 10:46:53 -0800

    Player Followers

---
+++ Test Project/Assets/FollowPlayer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public List<Vector3> trail;
+    public int followDelay = 10;
+    void Update()
+    {
+        if (trail.Count >= followDelay)
+        {
+            if (Vector3.Distance(player.transform.position,trail[followDelay-1]) > 0.1f)
+            {
+                //Add previous player position to follow list while player is moving
+                trail.Add(player.transform.position);
+            }
+            else
+            {
+                //Stop moving when player is not moving
+            }
+        }
+        else
+        {
+            //Set up list during the first frames
+            trail.Add(player.transform.position);
+        }
+        //Set position
+        if (trail.Count > followDelay)
+        {
+            trail.RemoveAt(0);
+            transform.position = trail[0];
+        }
+        
+    }
+}
--- Test Project/Assets/PlayerController.cs
+++ Test Project/Assets/PlayerController.cs
@@ -7,10 +7,7 @@ public class PlayerController : MonoBehaviour
     public Vector2 movement = new Vector2(0,0);
     public float speed = 5.0f;
     public float turnSpeed = 20.0f;
-    public float dashMultiplier = 10;
-    public float dashCooldown = 10;
-    public float dashLength = 5;
-    private float dashFrame = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,14 +19,11 @@ public class PlayerController : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        if (Input.GetButtonDown("Jump"))
-        {
-            dashFrame = dashCooldown;
-        }
+        
     }
     void FixedUpdate()
     {
-        dashFrame -= 1;
+        
         float angle = Vector2.SignedAngle(Vector2.right,movement);
         
         Vector3 targetRotation = new Vector3(0, 0, angle);
@@ -39,14 +33,9 @@ public class PlayerController : MonoBehaviour
             targetRotation = new Vector3(0, 0, angle);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation),360);
         }
-        if (dashFrame > dashCooldown-dashLength)
-        {
-            GetComponent<Rigidbody2D>().velocity = movement*speed*dashMultiplier;
-        }
-        else
-        {
-            GetComponent<Rigidbody2D>().velocity = movement*speed;
-        }
+        
+        GetComponent<Rigidbody2D>().velocity = movement*speed;
+        
         
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), turnSpeed);
         if (movement == new Vector2(0,0)) {
