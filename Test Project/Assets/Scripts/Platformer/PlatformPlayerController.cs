2564ceadd84a5c1873252a8f3e15d0d8156a4389
MysteriousShep <95491119+MysteriousShep@users.noreply.github.com>
2023-02-09 10:38:14 -0800

    Rethink Game Concept
    - Remove room for first boss
    - Remove dash
    - Add platformer level
    - Change player sprite
    - Remove currency

---
+++ Test Project/Assets/Scripts/Platformer/PlatformPlayerController.cs
@@ -0,0 +1,265 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public Vector2 movement;
+    public Animator playerAnimator;
+    private float yVelocity;
+    private int coyoteFrame = 0;
+    private bool grounded = false;
+    private int jumpFrame = 0;
+    public float jumpSpeed = 10.0f;
+    public int jumpDuration = 20;
+    public float gravity;
+    public int coyoteTime = 0;
+    public int accelleration = 3;
+    public int deccelleration = 3;
+    private float xVelocity = 0;
+    private float xSpeed = 0;
+    private Collider2D hit;
+    public List<GameObject> hair;
+    public GameObject hairObject;
+    public int hairLength = 6;
+    public Sprite hairSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerAnimator = GetComponent<Animator>();
+        hair.Add(Instantiate(hairObject,new Vector3(transform.position.x,transform.position.y-0.1f,transform.position.z-1),transform.rotation));
+        hair[0].GetComponent<HairFollow>().player = gameObject;
+        hair[0].GetComponent<HairFollow>().first = true;
+        for (int i = 1; i < hairLength; i++)
+        {
+            hair.Add(Instantiate(hairObject,new Vector3(transform.position.x,transform.position.y-i*0.1f,transform.position.z-1),transform.rotation));
+            hair[i].GetComponent<HairFollow>().player = hair[i-1];
+            hair[i].GetComponent<HairFollow>().index = i/4+2;
+            if (i > hairLength-3)
+            {
+                hair[i].GetComponent<SpriteRenderer>().sprite = hairSprite;
+            }
+        }
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Jump");
+        if (movement.x < 0)
+        {
+            for (int i = 0; i < hairLength; i++)
+            {
+                hair[i].transform.localScale = new Vector3(-0.5f,0.5f,1);
+            }
+            
+            
+        }
+        if (movement.x > 0)
+        {
+            for (int i = 0; i < hairLength; i++)
+            {
+                hair[i].transform.localScale = new Vector3(0.5f,0.5f,1);
+            }
+            
+        }
+        
+    }
+    
+    void FixedUpdate()
+    {
+        
+        
+        GetHitBoxAtPosition(transform.position.x,transform.position.y+yVelocity,0.35f,0.5f);
+
+        
+
+        grounded = false;
+        if (hit != null && hit.gameObject != gameObject) 
+        {
+            
+            
+            GetHitBoxAtPosition(transform.position.x,transform.position.y,0.35f,0.5f);
+            for (int i = 0; i < 30; i++)
+            {
+                if (hit == null) 
+                {
+                    transform.Translate(new Vector3(0,0.01f*Mathf.Sign(yVelocity),0),Space.World);
+                    GetHitBoxAtPosition(transform.position.x,transform.position.y,0.35f,0.5f);
+                }
+            }
+            yVelocity = 0;
+            GetHitBoxAtPosition(transform.position.x,transform.position.y,0.45f,0.4f);
+            if (hit != null && hit.gameObject != gameObject)
+            {
+                grounded = true;
+            }
+            else
+            {
+                jumpFrame = jumpDuration+1;
+                coyoteFrame = coyoteTime+1;
+            }
+        }
+        
+        yVelocity -= gravity*Time.fixedDeltaTime;
+        if (grounded) 
+        {
+            jumpFrame = 0;
+            coyoteFrame = 0;
+            yVelocity = 0;
+            
+                if (xSpeed != 0)
+                {
+                    //playerAnimator.SetTrigger("Run");
+                    
+                }
+                else
+                {
+                    //playerAnimator.SetTrigger("Idle");
+                }
+        }
+        else
+        {
+            if (yVelocity < -0.05f)
+            {
+                //playerAnimator.SetTrigger("Fall");
+            }
+            else if (yVelocity > 0.05f)
+            {
+                //playerAnimator.SetTrigger("Jump");
+            }
+            else
+            {
+                //playerAnimator.SetTrigger("Hang");
+            }
+        }
+        if (jumpFrame < jumpDuration && movement.y > 0 && coyoteFrame < coyoteTime) 
+        {
+            jumpFrame += 1;
+            yVelocity = jumpSpeed;
+            //playerAnimator.SetTrigger("Jump");
+            grounded = false;
+        } 
+        else if (jumpFrame < jumpDuration && (jumpFrame > 0 || coyoteFrame >= coyoteTime) && !grounded) 
+        {
+            jumpFrame += 2;
+        }
+        else if (!grounded && coyoteFrame < coyoteTime)
+        {
+            coyoteFrame += 1;
+        }
+        transform.Translate(new Vector3(0,yVelocity,0),Space.World);
+        
+        xVelocity -= xSpeed;
+        
+        if (movement.x != 0)
+        {
+            if (movement.x < 0)
+            {
+                if (xSpeed > 0)
+                {
+                    xSpeed -= speed/accelleration;
+                }
+                if (xSpeed > -speed)
+                {
+                    xSpeed -= speed/accelleration;
+                }
+            }
+            else
+            {
+                if (xSpeed < 0)
+                {
+                    xSpeed += speed/accelleration;
+                }
+                if (xSpeed < speed)
+                {
+                    xSpeed += speed/accelleration;
+                }
+            }
+        }
+        else
+        {
+            if (xSpeed < 0)
+            {
+                xSpeed += speed/deccelleration;
+                if (xSpeed > 0)
+                {
+                    xSpeed = 0;
+                }
+            }
+            if (xSpeed > 0)
+            {
+                xSpeed -= speed/deccelleration;
+                if (xSpeed < 0)
+                {
+                    xSpeed = 0;
+                }
+            }
+        }
+        if (xVelocity < 0)
+        {
+            xVelocity += speed/deccelleration;
+            if (xVelocity > 0)
+            {
+                xVelocity = 0;
+            }
+        }
+        if (xVelocity > 0)
+        {
+            xVelocity -= speed/deccelleration;
+            if (xVelocity < 0)
+            {
+                xVelocity = 0;
+            }
+        }
+        
+        if (xSpeed < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (xSpeed > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        //playerAnimator.SetFloat("xSpeed",Mathf.Abs(xSpeed/speed));
+        
+        xVelocity += xSpeed;
+        GetHitBoxAtPosition(transform.position.x+xVelocity,transform.position.y,0.25f,0.4f);
+        if (hit != null && hit.gameObject != gameObject) 
+        {
+            
+            
+            GetHitBoxAtPosition(transform.position.x,transform.position.y,0.25f,0.4f);
+            for (int i = 0; i < 30; i++)
+            {
+                if (hit == null) 
+                {
+                    transform.Translate(new Vector3(0.01f*Mathf.Sign(xVelocity),0,0));
+                    GetHitBoxAtPosition(transform.position.x,transform.position.y,0.25f,0.4f);
+                    
+                }
+            }
+            transform.Translate(new Vector3(-0.01f*Mathf.Sign(xVelocity),0,0));
+            xVelocity = 0;
+            xSpeed = 0;
+        }
+        transform.Translate(new Vector3(xVelocity,0,0));
+        
+        
+    }
+
+    void GetHitBoxAtPosition(float x, float y,float minHeight = 0.25f, float maxHeight = 1.0f)
+    {
+        Vector2 max = new Vector2(x+0.45f,y+maxHeight);
+        Vector2 min = new Vector2(x-0.45f,y-minHeight);
+        hit = Physics2D.OverlapArea(max,min);
+        Debug.DrawLine(new Vector3(max.x,max.y,0),new Vector3(min.x,max.y,0),Color.red);
+        Debug.DrawLine(new Vector3(min.x,max.y,0),new Vector3(min.x,min.y,0),Color.red);
+        Debug.DrawLine(new Vector3(min.x,min.y,0),new Vector3(max.x,min.y,0),Color.red);
+        Debug.DrawLine(new Vector3(max.x,min.y,0),new Vector3(max.x,max.y,0),Color.red);
+    }
+
+
+}
--- Test Project/Assets/Scripts/Player/PlayerController.cs
+++ Test Project/Assets/Scripts/Player/PlayerController.cs
@@ -11,11 +11,6 @@ public class PlayerController : MonoBehaviour
     public int hairLength = 6;
     public Sprite sprite;
     public Sprite hairSprite;
-    public bool hasDash;
-    public Sprite dashSprite;
-    public int dashFrame = 0;
-    public int dashCooldown = 20;
-    public float dashSpeed = 10;
 
     void Start() 
     {
@@ -37,76 +32,9 @@ public class PlayerController : MonoBehaviour
     // Every frame
     void Update()
     {
-        if (dashFrame <= 0)
-        {
             
-            movement.x = Input.GetAxisRaw("Horizontal"); // Set movement X to HORIZONTAL input
-            movement.y = Input.GetAxisRaw("Vertical"); // Set movement Y to VERTICAL input
-            if (Input.GetButtonDown("Jump") && hasDash)
-            {
-                dashFrame = dashCooldown;
-                if (movement.x == 0 && movement.y == 0) 
-                {
-                    movement.x = Mathf.Sign(transform.localScale.x);
-                }
-            }
-            for (int i = 0; i < hairLength; i++)
-            {
-                hair[i].GetComponent<SpriteRenderer>().color = new Color(1,1,1,1);
-            }
-            GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else
-        {
-            dashFrame -= 1;
-            hair[0].GetComponent<SpriteRenderer>().color = new Color(1,0,0,1);
-            
-            hair[0].GetComponent<HairFollow>().trail[1] = transform.position;
-            hair[0].transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z+1);
-            hair[0].transform.Translate(movement*dashSpeed*Time.deltaTime*1/3);
-            if (dashFrame <= 0)
-            {
-                hair[0].GetComponent<HairFollow>().trail.RemoveAt(0);
-                hair[0].GetComponent<HairFollow>().trail.RemoveAt(0);
-                hair[0].transform.position = new Vector3(transform.position.x,transform.position.y-0.1f,transform.position.z+1);
-                for (int j = 0; j < hair[0].GetComponent<HairFollow>().followDelay; j++)
-                {
-                    hair[0].GetComponent<HairFollow>().trail.Add(transform.position);
-                }
-            }
-            
-            for (int i = 1; i < hairLength; i++)
-            {
-                if (dashFrame > -5) {
-                    hair[i].GetComponent<SpriteRenderer>().color = new Color(1,0,0,1);
-                    
-                    hair[i].GetComponent<HairFollow>().trail[1] = hair[i].transform.position;
-                    hair[i].transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z+1);
-                    hair[i].transform.Translate(movement*dashSpeed*-Time.deltaTime*i/4);
-                    
-                    hair[i].GetComponent<HairFollow>().trail[0] = hair[i].transform.position;
-                    hair[i].GetComponent<HairFollow>().trail[1] = hair[i].transform.position;
-                    //hair[i].GetComponent<HairFollow>().trail[2] = hair[i-1].transform.position;
-                }
-                else
-                {
-                    /*
-                    hair[i].GetComponent<HairFollow>().trail.RemoveAt(0);
-                    hair[i].GetComponent<HairFollow>().trail.RemoveAt(0);
-                    hair[i].GetComponent<HairFollow>().trail.RemoveAt(0);
-                    
-                    hair[i].transform.position = new Vector3(transform.position.x,transform.position.y-0.1f*i,transform.position.z);
-                    for (int j = 0; j < hair[i].GetComponent<HairFollow>().followDelay; j++)
-                    {
-                        hair[i].GetComponent<HairFollow>().trail.Add(hair[i].GetComponent<HairFollow>().player.transform.position);
-                    }
-                    */
-                }
-                
-            }
-            GetComponent<SpriteRenderer>().sprite = dashSprite;
-        }
-            
+        movement.x = Input.GetAxisRaw("Horizontal"); // Set movement X to HORIZONTAL input
+        movement.y = Input.GetAxisRaw("Vertical"); // Set movement Y to VERTICAL input
         if (movement.x < 0)
         {
             for (int i = 0; i < hairLength; i++)
@@ -114,7 +42,6 @@ public class PlayerController : MonoBehaviour
                 hair[i].transform.localScale = new Vector3(-0.5f,0.5f,1);
             }
             transform.localScale = new Vector3(-0.75f,0.75f,1);
-            
         }
         if (movement.x > 0)
         {
@@ -129,13 +56,6 @@ public class PlayerController : MonoBehaviour
     // Every 60th of a second
     void FixedUpdate()
     {
-        if (dashFrame <= 0)
-        {
-            GetComponent<Rigidbody2D>().velocity = movement*speed; // Move player based on input
-        }
-        else
-        {
-            GetComponent<Rigidbody2D>().velocity = movement*dashSpeed; // Move player based on input
-        }
+        GetComponent<Rigidbody2D>().velocity = movement*speed; // Move player based on input
     }
 }
